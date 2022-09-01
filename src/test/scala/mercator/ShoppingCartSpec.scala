eb76f277088ec5f3c621b622899ce1a5e2acb311
hs3t <4068568+hs3t@users.noreply.github.com>
2022-09-01 14:28:08 +0100

    Offers tests added

--- src/test/scala/mercator/ShoppingCartSpec.scala
+++ src/test/scala/mercator/ShoppingCartSpec.scala
@@ -5,11 +5,29 @@ import org.scalatest.matchers.should.Matchers
 
 class ShoppingCartSpec extends AnyFlatSpec with Matchers {
 
-  "Shopping Cart" should "return total price of zero for empty cart" in {
+  behavior of "Shopping Cart"
+
+  it should "return total price of zero for empty cart" in {
     ShoppingCart.checkout() shouldBe 0
   }
 
-  "Shopping Cart" should "return total price when given list of items" in {
-    ShoppingCart.checkout("Apple", "Apple", "Orange", "Apple") shouldBe 2.05
+  it should "return total price when given the list of items" in {
+    ShoppingCart.checkout("Orange", "Apple", "Orange") shouldBe 1.10
+  }
+
+  it should "return total price when buy one get one free Apple offer applied" in {
+    ShoppingCart.checkout("Apple", "Apple", "Orange", "Apple") shouldBe 1.45
+  }
+
+  it should "return total price when three for two Oranges offer applied" in {
+    ShoppingCart.checkout("Orange", "Orange", "Orange", "Apple") shouldBe 1.10
+  }
+
+  it should "return total price when both offers applied" in {
+    ShoppingCart.checkout("Orange", "Orange", "Orange", "Apple", "Apple") shouldBe 1.10
+  }
+
+  it should "return total price when both offers applied on multiple items" in {
+    ShoppingCart.checkout("Orange", "Orange", "Orange", "Orange", "Apple", "Apple", "Apple") shouldBe 1.95
   }
 }
